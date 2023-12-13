caa4bbd44286f99f81a23d8dc7f51f14efef196a
vikiru <72267229+vikiru@users.noreply.github.com>
2023-12-13 18:38:58 -0500

    Load BDD background data - Ex 4

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -47,6 +47,12 @@ def step_impl(context):
     # load the database with new products
     #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
